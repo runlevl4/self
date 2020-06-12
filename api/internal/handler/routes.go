718a1cf3502cde38bd9f8aea13b6491793eaebef
runlevl4 <root@runlevl4.com>
2020-06-11 23:06:33 -0400

    Tweaked /info to return k8s details

--- api/internal/handler/routes.go
+++ api/internal/handler/routes.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
 const content = "Content-Type"
@@ -15,11 +18,45 @@ func Health(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `{"status": "HEALTHY"}`)
 }
 
+// InfoResponse provides the response details for the Info route.
+type InfoResponse struct {
+	Status    string `json:"status,omitempty"`
+	Pod       string `json:"pod,omitempty"`
+	PodIP     string `json:"pod_ip,omitempty"`
+	Node      string `json:"node,omitempty"`
+	Namespace string `json:"namespace,omitempty"`
+}
+
 //Info responds if the service is up.
 func Info(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set(content, contentJSON)
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, `{"status": "UP"}`)
+	// fmt.Printf("HOSTNAME: %s\n", os.Getenv("HOSTNAME"))
+	// fmt.Fprintf(w, `{"status": "UP"}`)
+	cmd, _ := exec.LookPath("hostname")
+	h := &exec.Cmd{
+		Path: cmd,
+		Args: []string{cmd},
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	fmt.Printf("host: %v\n", h.String())
+	// Build response
+	host, _ := os.Hostname()
+	ir := InfoResponse{
+		Status: "UP",
+		// Pod:       os.Getenv("HOSTNAME"),
+		Pod:       host,
+		PodIP:     os.Getenv("KUBERNETES_NAMESPACE_POD_IP"),
+		Node:      os.Getenv("KUBERNETES_NODENAME"),
+		Namespace: os.Getenv("KUBERNETES_NAMESPACE"),
+	}
+	rs, err := json.Marshal(ir)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, `{"error": "unable to parse info"}`)
+	}
+	fmt.Fprintf(w, string(rs))
 }
 
 // Hello is our simple entrypoint.
